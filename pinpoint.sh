fe80b44804675cd2adfdc6c4457f26390d039bff
Alex Narvey <anarvey@precursor.ca>
2025-08-22 07:11:36 -0500

    Update pinpoint.sh
    Added -n to help usage options

--- pinpoint.sh
+++ pinpoint.sh
@@ -40,7 +40,7 @@ rundate=`date -u +%Y-%m-%d\ %H:%M:%S\ +0000`
 # help information
 usage()
 {
-    echo "usage: $scriptname [-V] [-j] [-h] [-g] [-a] [-k yourkeyhere] [-d] [-o]
+    echo "usage: $scriptname [-V] [-j] [-h] [-g] [-a] [-k yourkeyhere] [-d] [-n] [-o]
 	-V | --version		Print script version and exit
 	-j | --jamf		Return map URL only to stdout formatted as an
     				extension attribute for use with JAMF Pro
